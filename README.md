c070d90a7f301d7a5915ca75ceb4a61fc4defdf5
eliglezz <eliglezz@users.noreply.github.com>
2021-09-28 22:37:41 -0700

    most if not all should be done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # api-code-quiz
+Another homework assignment, the goal was to have a short quiz that punished a user for getting an incorrect answer, have a countdown timer, allowed the user to add their score to the page using local storage, had button to start, and provide multiple questions. All this had to be achieved with JavaScript. The code consist of a list of buttons showing when pressing start, going into functions to keep the next questions. Event listeners were added to pick up button clicks. I found this challenging and not sure it works correctly yet.
--- script.js
+++ script.js
@@ -1,14 +1,83 @@
-var seconds = 60;
 var score = 0;
 var highScore;
+var timerEl = document.getElementById("countdown");
+var input = document.getElementById("initials");
+questionEl = document.querySelector("question");
+instructionEl = document.querySelector("instructions")
+answer1El = document.querySelector("#answer1");
+answer2El = document.querySelector("#answer2");
+answer3El = document.querySelector("#answer3");
+answer4El = document.querySelector("#answer4");
+hs1El = document.querySelector("#hs1");
+hs2El = document.querySelector("#hs2");
+hs3El = document.querySelector("#hs3");
+// <= defunct array I first tried to Use=>
+// var questions = [
+//   {
+//     question: "What is the distance between Earth and The Moon?",
+//     answers: {
+//       a: "The Moon’s distance from Earth is about 300,000 miles (383,000km).",
+//       b: "The Moon’s distance from Earth is about 340,000 miles (547,000km).",
+//       c: "The Moon’s distance from Earth is about 240,000 miles (385,000km).",
+//       d: "The Moon’s distance from Earth is about 200,000 miles (321,000km).",
+//     },
+//     correctAns: "c",
+//   },
+//   {
+//     question: "Where does the Moon get its light from?",
+//     answers: {
+//       a: "The Sun",
+//       b: "The Moon's atmosphere",
+//       c: "The Moon's Core",
+//       d: "The Earth",
+//     },
+//     correctAns: "a",
+//   },
+//   {
+//     question: "How much of Moon's Surface is usually lit up?",
+//     answers: {
+//       a: "25%",
+//       b: "100%",
+//       c: "75%",
+//       d: "50%",
+//     },
+//     correctAns: "d",
+//   },
+//   {
+//     question: "Approximately how wide is The Moon",
+//     answers: {
+//       a: "The Moon is about 2160 miles (3475km) wide.",
+//       b: "The Moon is about 2260 miles (3640km) wide.",
+//       c: "The Moon is about 3160 miles (5090km) wide.",
+//       d: "The Moon is about 3260 miles (5250km) wide.",
+//     },
+//     correctAns: "a",
+//   },
+// ];
 
 // <= countdown function =>
-var seconds = document.getElementById("countdown").textContent;
-var countdown = setInterval(function () {
-  seconds--;
-  document.getElementById("countdown").textContent = seconds;
-  if (seconds <= 0) clearInterval(countdown);
-}, 1000);
+// Timer that counts down from 5
+function countdown() {
+  var timeLeft = 60;
+
+  // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show the remaining seconds
+      timerEl.textContent = "Time: " + timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+      // Once `timeLeft` gets to 0, set `timerEl` to an empty string
+      timerEl.textContent = "";
+      // Use `clearInterval()` to stop the timer
+      clearInterval(timeInterval);
+      // Call the `displayMessage()` function
+      stopGame();
+    }
+  }, 1000);
+}
 
 // <= event listener for submit =>
 var submitBtn = document.createElement("button");
@@ -16,19 +85,13 @@ submitBtn.innerHTML = "Submit";
 submitBtn.addEventListener("click", function () {
   console.log("This works");
   if (score > 0) {
-    // store score
+    storeScore();
+  } else {
+    alert("You at least need to get one right!");
   }
 });
-document.body.appendChild(submitBtn); // change to where the button should be
+document.getElementById("submitBtn").appendChild(submitBtn); // change to where the button should be
 
-// <= button to start game =>
-var startBtn = document.createElement("button");
-startBtn.innerHTML = "Start Game";
-startBtn.addEventListener("click", function () {
-  console.log("This works");
-  startGame();
-});
-document.body.appendChild(startBtn); // change to where the button should be
 
 // <= go back button =>
 var goBack = document.createElement("button");
@@ -37,4 +100,94 @@ goBack.addEventListener("click", function () {
   console.log("This works");
   reset(); // add reset function
 });
-document.body.appendChild(goBack); // change to where the button should be
+document.getElementById("goBack").appendChild(goBack); // change to where the button should be
+
+// <= adds score to local storage =>
+function submitScore(event) {
+  localStorage.setItem("score", JSON.stringify(score));
+  localStorage.setItem("initials", JSON.stringify(initials));
+}
+
+function showScore() {
+  // Get stored stats from localStorage
+  var score = JSON.parse(localStorage.getItem("score"));
+  var initials = JSON.parse(localStorage.getItem("initials"));
+}
+
+// <= button to start game =>
+var startBtn = document.createElement("button");
+startBtn.innerHTML = "Start Game";
+startBtn.addEventListener("click", function startQuiz() {
+  console.log("This works");
+  countdown();
+  question1();
+});
+document.getElementById("startBtn").appendChild(startBtn); // change to where the button should be
+
+function question1(){
+  
+  questionEl.textContent = "What is the distance between Earth and The Moon?"
+  answer1El.addEventListener("click") = "The Moon’s distance from Earth is about 300,000 miles (383,000km)."
+  answer2El.addEventListener("click") = "The Moon’s distance from Earth is about 340,000 miles (547,000km)."
+  answer3El.addEventListener("click") = "The Moon’s distance from Earth is about 240,000 miles (385,000km)."
+  answer4El.addEventListener("click") = "The Moon’s distance from Earth is about 200,000 miles (321,000km)."
+  if ( answer3El) {
+    score+=15;
+    question2();
+  } else {
+    timeLeft-=15;
+    question2();
+  }
+}
+
+function question2(){
+  
+  questionEl.textContent = "Where does the Moon get its light from?"
+  answer1El.addEventListener("click") = "The Sun"
+  answer2El.addEventListener("click") = "The Moon's atmosphere"
+  answer3El.addEventListener("click") = "The Moon's Core"
+  answer4El.addEventListener("click") = "The Earth"
+  if ( answer1El) {
+    score+=15;
+    question3();
+  } else {
+    timeLeft-=15;
+    question3();
+  }
+}
+
+function question3(){
+  
+  questionEl.textContent = "How much of Moon's Surface is usually lit up?"
+  answer1El.addEventListener("click") = "25%",
+  answer2El.addEventListener("click") = "100%"
+  answer3El.addEventListener("click") = "75%"
+  answer4El.addEventListener("click") = "50%"
+  if ( answer4El) {
+    score+=15;
+    question4();
+  } else {
+    timeLeft-=15;
+    question4();
+  }
+}
+
+function question4(){
+  
+  questionEl.textContent = "Approximately how wide is The Moon"
+  answer1El.addEventListener("click") = "The Moon is about 2160 miles (3475km) wide."
+  answer2El.addEventListener("click") = "The Moon is about 2260 miles (3640km) wide."
+  answer3El.addEventListener("click") = "The Moon is about 3160 miles (5090km) wide."
+  answer4El.addEventListener("click") = "The Moon is about 3260 miles (5250km) wide."
+  if ( answer1El) {
+    score+=15;
+    stopGame();
+  } else {
+    timeLeft-=15;
+    stopGame();
+}
+}
+
+function stopGame() {
+  time
+}
